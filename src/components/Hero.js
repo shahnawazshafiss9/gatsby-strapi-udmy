fba0fc8f46133560d83aa9430d773c0e6356d6f7
Shahnawaz Alam <shahnawazshafiss9@gmail.com>
2021-01-25 15:07:33 +0530

    nav working home page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,33 @@ import SocialLinks from "../constants/socialLinks"
 // ...GatsbyImageSharpFluid
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  const { file: { childImageSharp: { fluid } } } = useStaticQuery(query);
+  return (<header className="hero">
+    <div className="section-center hero-center">
+      <article className="hero-info">
+        <div className="underline"></div>
+          <h1>I'm Shahnawaz</h1>
+          <h4>Web and Mobile UI/UX developer</h4>
+          <Link to="/contact" className="btn">Contact me</Link>
+          <SocialLinks />
+      </article>
+      <Image fluid={fluid} className="hero-img" />
+    </div>
+  </header>
+
+  );
 }
+
+const query = graphql`
+  {
+    file(relativePath: {eq: "hero-img.png"}) {
+      id
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }`
 
 export default Hero
